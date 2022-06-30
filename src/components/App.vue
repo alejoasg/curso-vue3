f6130d0baa95939040631be27257aa51de6c3cca
Alvaro Muñoz Garcia <amgarcia@arsys.es>
2022-06-30 09:25:00 +0200

    Renderizado

--- src/components/App.vue
+++ src/components/App.vue
@@ -14,6 +14,31 @@
             ComponenteP  : ComponenteP,
         },
 
+        data() {
+
+            return  {
+
+                h1 : "Hello world!!",
+                p  : "Good bye",
+
+            };
+
+        },
+
+        methods : {
+
+            /* Ojo con esto, ¡problemas con el this y las funciones arrow!*/
+            // click : () {
+            /* Una de estas dos es la buena */
+            // click : function () {
+            doClick() {
+
+                this.h1 =  "Hello world!! " + Math.random();
+                this.p =  "Good bye " + Math.random();
+
+            },
+        },
+
     } );
 
 </script>
@@ -22,8 +47,13 @@
 
     <div>
 
-        <ComponenteH1 text="Hello world (prop)!!" />
-        <ComponenteP text="Good bye (prop)"> </ComponenteP>
+        <p>Cambia el valor para el h1 y el p<button v-on:click="doClick()">Click</button></p>
+
+        <ComponenteH1 text="Hello world!!" />
+        <ComponenteP text="Good bye"> </ComponenteP>
+
+        <ComponenteH1 v-bind:text="h1" />
+        <ComponenteP v-bind:text="p"> </ComponenteP>
 
     </div>
 
